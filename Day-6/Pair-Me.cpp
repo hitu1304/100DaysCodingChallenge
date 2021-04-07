4a82ae9b96136a709b46c8ccac6f2b51785b02d9
hitu1304 <hiteshgendre2000@gmail.com>
2021-04-07 19:15:35 +0530

    Day-6

---
+++ Day-6/Pair-Me.cpp
@@ -0,0 +1,69 @@
+/*plateform-> Codechef(div-2 rated)
+    contest name-> AprilChallenge2020
+    language used->cpp.
+    
+    Problem Statement->
+
+Chef is always trying to find patterns within integers. Today Chef has 3 positive integers and he wonders if any one of them can be written as the sum of the other two integers.
+
+Input:
+First line will contain the number 5, which is the number of testcases. Then the testcases follow.
+Each testcase contains a single line of input, with three space-separated positive integers x, y, and z.
+Output:
+For each testcase, output in a single line, "YES" if it's possible to represent any integer among the three integers as the sum of the other two integers, and "NO" if not.
+
+NOTE: The output is case insensitive, meaning both uppercase and lowercase characters can be used at any position of the output string.
+
+Constraints
+1<=x,y,z<=1000
+Subtasks
+100% points - Original constraints
+Sample Input:
+5
+1 1 2
+1 3 2
+2 2 2
+100 100 201
+11 22 33
+Sample Output:
+YES
+YES
+NO
+NO
+YES
+Explanation:
+Case 1: For this case, the third integer can be written as the sum of the first two. 2=1+1
+Case 2: For this case, the second integer can be written as the sum of the first and third. 3=1+2
+Case 3: For this case, no integer can be written as the sum of other two.
+
+Case 4: For this case, no integer can be written as the sum of other two.
+
+Case 5: For this case, the third integer can be written as the sum of the first two. 33=11+22
+
+Approach-> brute force*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	
+	int t,x,y,z;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>x>>y>>z;
+	   int b= max(y,z);
+	     int c= max(x,z);
+	     int a=max(b,c);
+	    if(x+y==a ||y+z==a || z+x==a)
+	    {
+	        cout<<"yes"<<endl;
+	    }else
+	    cout<<"no"<<endl;
+	    
+	}
+	return 0;
+}
+
+//Time complexity-> constant 
