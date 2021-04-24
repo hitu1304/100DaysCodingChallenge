11e8a9afec176af022c7f1cf8725eeb86915672a
hitu1304 <hiteshgendre2000@gmail.com>
2021-04-24 17:18:34 +0530

    Day-23

---
+++ Day-23/Floor-in-a-Sorted-Array.cpp
@@ -0,0 +1,63 @@
+/*plateform-> GeeksForGeeks (practice Easy)
+
+Topic -> Linked list.
+
+language used->cpp.
+    
+Problem Statement->
+
+
+Given a sorted array arr[] of size N without duplicates, and given a value x. Floor of x is defined as the largest element K in arr[] such that K is smaller than or equal to x. Find the index of K(0-based indexing).
+
+Example 1:
+
+Input:
+N = 7, x = 0 
+arr[] = {1,2,8,10,11,12,19}
+Output: -1
+Explanation: No element less 
+than 0 is found. So output 
+is "-1".
+Example 2:
+
+Input:
+N = 7, x = 5 
+arr[] = {1,2,8,10,11,12,19}
+Output: 1
+Explanation: Largest Number less than 5 is
+2 (i.e K = 2), whose index is 1(0-based 
+indexing).
+Your Task:
+The task is to complete the function findFloor() which returns an integer denoting the index value of K or return -1 if there isn't any such number.
+
+Expected Time Complexity: O(log N).
+Expected Auxiliary Space: O(1).
+
+Constraints:
+1 ≤ N ≤ 107
+1 ≤ arr[i] ≤ 1018
+0 ≤ X ≤ arr[n-1]
+
+
+Approach-> Find the postion of the first value that is less greater than x.*/
+
+
+class Solution{
+  public:
+    // Function to find floor of x
+    // n: size of vector
+    // x: element whose floor is to find
+    int findFloor(vector<long long> v, long long n, long long x){
+        
+        // Your code here
+        int i=n-1;
+        int mi=*min_element(v.begin(),v.end());
+        while(v[i]>x)
+        {
+            i--;
+        }if(mi<=x)return i;return -1;
+        
+    }
+};
+
+//Time Complexity: O(log N).
