4ffb89d69d2e31f9abba5877925d04313c196f95
hitu1304 <hiteshgendre2000@gmail.com>
2021-05-05 19:07:55 +0530

    Day-35

---
+++ Day-35/Count-BST-nodes-that-lie-in-a-given-range.cpp
@@ -0,0 +1,67 @@
+/*plateform-> GeeksForGeeks (practice Medium)
+
+Topic -> Binary Search Tree.
+
+language used->cpp.
+    
+Problem Statement->
+
+Given a Binary Search Tree (BST) and a range l-h(inclusive), count the number of nodes in the BST that lie in the given range.
+
+The values smaller than root go to the left side
+The values greater and equal to the root go to the right side
+Example 1:
+
+Input:
+      10
+     /  \
+    5    50
+   /    /  \
+  1    40  100
+l = 5, h = 45
+Output: 3
+Explanation: 5 10 40 are the node in the
+range
+Example 2:
+
+Input:
+     5
+    /  \
+   4    6
+  /      \
+ 3        7
+l = 2, h = 8
+Output: 5
+Explanation: All the nodes are in the
+given range.
+Your Task:
+This is a function problem. You don't have to take input. You are required to complete the function getCountOfNode() that takes root, l ,h as parameters and returns the count.
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(Height of the BST).
+
+Constraints:
+1 <= Number of nodes <= 100
+1 <= l < h < 103
+
+Approach-> traverse through th tree and keep count the node lies in the given range*/
+
+int getCount(Node *root, int l, int h)
+{
+  // your code goes here   
+    Node *p=root;
+    int x,y;
+    if(p)
+     {
+       x=getCount(root->left,  l,  h);
+       y=getCount(root->right,  l,  h);
+       if(root->data>=l && root->data<=h)
+       {
+          return x+y+1;
+       }else
+       {
+          return x+y;
+       }
+    }
+}
+//Time Complexity: O(N)
